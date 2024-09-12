d980b9b70a9a56a28f24917da2e2703dd7a7b282
Drew Richards <98420057+darichards10@users.noreply.github.com>
2024-09-12 18:04:17 -0400

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,37 @@
-import Link from "next/link"
 
-import { appClient } from "@/lib/auth0"
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { Auth0Logo } from "@/components/auth0-logo"
 
-import { SignUpForm } from "./signup-form"
-import { WelcomeBackCard } from "./welcome-back-card"
+import { appClient } from "@/lib/auth0";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { Auth0Logo } from "@/components/auth0-logo";
+
+import { Button, Card, CardHeader, CardBody, CardFooter, Link } from "@nextui-org/react";
+
 
 export default async function Home() {
   const session = '';//await appClient.getSession()
 
   return (
     <div className="container relative sm:grid h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      {session ? (
-        <a
-          href="/api/auth/logout"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Logout
-        </a>
-      ) : (
-        <div
-          className="absolute right-4 top-4 md:right-8 md:top-8"
-        ><span className="text-sm">Already joined?</span> <a
-          className="text-sm underline"
-          href="/api/auth/login"
-        >
-          Log in
-        </a>
-        </div>
-      )}
+      <Card className="max-w-lg w-full h-min">
+        <CardHeader className="flex justify-center items-center">
+          <i className="fa fa-car mr-3 text-2xl text-blue"></i>
+          <h3 className="text-xl font-semibold">Compare a Vehicle</h3>
+        </CardHeader>
 
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-black" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <Auth0Logo className="mr-2 size-8" />
-          <span className="font-semibold">SaaStart</span>
-        </div>
-        <div className="relative z-20 m-auto max-w-sm text-center">
-          <blockquote className="space-y-2">
-            <div className="space-y-8">
-              <p className="text-lg font-medium">
-                SaaStart is a reference B2B SaaS application built using Next.js
-                and Auth0 by Okta.
-              </p>
-              <p className="text-lg">
-                It features multi-tenancy support, user management and access
-                controls, security policies, self-service Single Sign-On
-                configuration and more out-of-the-box.
-              </p>
-            </div>
-          </blockquote>
-        </div>
-      </div>
-      <div className="lg:p-8 flex h-screen">
-        {session ? <WelcomeBackCard /> : <SignUpForm />}
-      </div>
+        <CardBody className="text-center">
+          <p className="text-gray-700 mb-6">
+            Discover how a vehicle's previous listings compare to the current market. Click the button below to analyze any VIN and see trends, pricing, and more.
+          </p>
+        </CardBody>
+
+        <CardFooter className="flex justify-center">
+          <Button color="primary" as={Link} href="/vin/analysis">
+            <i className="fa fa-chart-line mr-3 text-2xl text-white"></i>
+            Try Now
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss"
 import { fontFamily } from "tailwindcss/defaultTheme"
+const { nextui } = require("@nextui-org/react");
+
 
 const config = {
   darkMode: ["class"],
@@ -24,6 +26,18 @@ const config = {
         mono: [...fontFamily.mono],
       },
       colors: {
+        blue: '#7695FF',
+        'light-blue': '#9DBDFF',
+        pale: '#FFD7C4',
+        orange: '#FF9874',
+        black: '#000000',
+        'gray-dark': '#1a1a1a',
+        gray: '#2d2d2d',
+        'gray-light': '#4a4a4a',
+        'gray-lighter': '#a1a1a1',
+        'gray-lightest': '#d4d4d4',
+        white: '#ffffff',
+        'off-white': '#f5f5f5',
         field: "hsl(var(--field))",
         hover: "hsl(var(--hover))",
         border: "hsl(var(--border))",
@@ -81,7 +95,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [nextui(), require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config
 
 export default config
