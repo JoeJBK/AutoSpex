eb76646773be649a37d3014c813f5d35ba30106c
Drew Richards <98420057+darichards10@users.noreply.github.com>
2024-09-12 19:23:48 -0400

    test

---
+++ app/api/market/history/route.js
@@ -0,0 +1,57 @@
+'use client';
+const API_KEY = process.env.MARKET_CHECK_KEY;
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+export async function GET(request) {
+  const url = new URL(request.url);
+
+  const params = new URLSearchParams(url.search);
+
+  const year = params.get('year');
+  const make = params.get('make');
+  const model = params.get('model');
+  const trim = params.get('trim');
+
+  const data = await salesHistory(year, make, model, trim);
+  return new Response(
+    JSON.stringify(data),
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+}
+
+//API Functions
+async function salesHistory(year = null, make = null, model = null, trim = null) {
+  try {
+    let url = `https://mc-api.marketcheck.com/v2/sales/car?api_key=${API_KEY}&ymmt=`;
+
+    if (year) {
+      url += `${encodeURIComponent(year)}`;
+    }
+    if (make) {
+      url += `|${encodeURIComponent(make)}`;
+    }
+    if (model) {
+      url += `|${encodeURIComponent(model)}`;
+    }
+    if (trim) {
+      url += `|${encodeURIComponent(trim)}`;
+    }
+    console.log(url);
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
---
+++ app/api/price/route.js
@@ -0,0 +1,41 @@
+// src/app/api/vin/history/route.js
+'use client';
+const API_KEY = process.env.MARKET_CHECK_KEY;
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+export async function GET(request) {
+  const url = new URL(request.url);
+  
+  const params = new URLSearchParams(url.search);
+  
+  const vin = params.get('vin');
+  const miles = params.get('miles');
+  const zip = params.get('zip');
+  const data = await priceHistory(vin, miles, zip)
+  return new Response(
+    JSON.stringify(data), 
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+}
+
+//API Functions
+async function priceHistory(vin, miles, zip, retries = 3) {
+  try {
+    const response = await fetch(`https://mc-api.marketcheck.com/v2/predict/car/price?api_key=${API_KEY}&vin=${vin}&zip=${zip}&car_type=used&miles=${miles}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.status === 429 && retries > 0) {
+      await delay(1000); 
+      return await priceHistory(vin, retries - 1);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return { error: err.message };
+  }
+}
