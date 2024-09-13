003ec0cae8df0b0b558779e90c21bc412b9035e3
Drew Richards <98420057+darichards10@users.noreply.github.com>
2024-09-13 11:44:05 -0400

    def fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-
-import "./globals.css";
+import "./globals.css"; // Tailwind CSS imports here
 
 import { Inter } from "next/font/google";
 import Script from "next/script";
 
 import { NextUIProvider } from '@nextui-org/react';
 import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "@/components/theme-provider";
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,19 +23,11 @@ export default async function RootLayout({
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning={true}>
       <body className="min-h-screen bg-off-white font-sans antialiased" suppressHydrationWarning={true}>
-        {/* <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        > */}
         <NextUIProvider>
           <Header />
           {children}
           <Footer />
         </NextUIProvider>
-
-        {/* </ThemeProvider> */}
 
         <Toaster position="bottom-right" />
       </body>
