af1d71196d6fdfac11855ad15315964f2bb33263
Drew Richards <98420057+darichards10@users.noreply.github.com>
2024-09-13 09:17:19 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,   // Enables React strict mode for highlighting potential issues
+  
+  // Optional: If using environment variables, ensure they are loaded correctly
+  env: {
+    MARKET_CHECK_KEY: process.env.MARKET_CHECK_KEY,  // Ensure your API key is available in Amplify
+  },
+ 
+};
 
 export default nextConfig;
