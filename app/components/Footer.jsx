88b3ce057edc3b05898c122984fc46971a37d1fd
Drew Richards <98420057+darichards10@users.noreply.github.com>
2024-09-15 13:52:06 -0400

    meh

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,26 +2,41 @@ import { Image, Input, Button } from "@nextui-org/react";
 
 export default function Footer() {
   return (
-    <footer className="bg-orange p-4">
-      <div className="flex flex-row gap-4 space-between text-white">
-        <div className="w-1/3">
-        <Image
-          src="/assets/imgs/whitelogo.png"
-        />
+    <footer className="bg-orange p-4 mt-12">
+      <div className="flex flex-row gap-4 text-white">
+        <div className="flex-1">
+          <Image
+            src="/assets/imgs/whitelogo.png"
+            alt="Logo"
+            className="w-full h-auto max-w-[300px]"
+          />
         </div>
-        
-        <div>
-          <h3 className="text-xl font-bold ">Stay in the know</h3>
-          <p>Join our newsletter to get the latest </p>
+
+        <div className="flex-1 flex flex-col justify-center">
+          <h3 className="text-lg font-bold">Stay in the know</h3>
+          <p className="text-sm">Join our newsletter to get the latest updates and offers.</p>
         </div>
-        <div className="flex flex-col lg:flex-row gap-2">
-          <Input type="email" label="Email" />
-          <Button className="bg-carbon text-white">
-            Subscribe
-          </Button>
+
+        <div className="flex-1 flex flex-col lg:flex-row gap-2">
+          <div className="flex-1 flex items-center">
+            <Input type="email" label="Email" className="flex-1" />
+          </div>
+          <div className="flex-none flex items-center">
+            <Button className="bg-carbon text-white w-full h-12 lg:h-12">
+              Subscribe
+            </Button>
+          </div>
         </div>
       </div>
-      <div className="text-center text-gray-600">
+
+      <div className="text-center p-6">
+        {/* {'todo'} */}
+      </div>
+
+      <div className="text-center mt-4">
+        <p className="text-sm text-white">
+          &copy; {new Date().getFullYear()} AutoSpex. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import { FaTools } from "react-icons/fa";
 import { VscGraphLine } from "react-icons/vsc";
 import { BsRobot } from "react-icons/bs";
 import { GrContact } from "react-icons/gr";
+import { FaChartLine } from "react-icons/fa";
+import { GrServices } from "react-icons/gr";
+import { IoBarChart } from "react-icons/io5";
 
 export default async function Home() {
 
   return (
-    <div className="space-y-4 pb-64">
+    <div className="space-y-4 pb-4">
       <div className="p-4 flex flex-col md:flex-row items center">
         <Image
           width="100%"
@@ -16,86 +19,97 @@ export default async function Home() {
           disableSkeleton={false}
           loading="lazy"
         />
-        <Card className="bg-transparent text-white">
-          <CardHeader>
-            <h2 className="text-xl font-bold">Cutting Edge Data Analytics</h2>
+        <Card className="bg-gradient-to-r from-carbon via-carbon to-cool-gray text-white shadow-lg rounded-lg p-6">
+          <CardHeader className="flex items-center space-x-4 border-b">
+            <FaChartLine className="text text-2xl" />
+            <h2 className="text-2xl font-bold">Cutting Edge Data Analytics</h2>
           </CardHeader>
-          <CardBody>
-            We use advanced AI and large automotive databases to help you find the best car for your budget. Our tools analyze real-time market data, trends, and vehicle specs to offer personalized recommendations. With this technology, car buying becomes faster, smarter, and tailored to your needs.           </CardBody>
-          <CardFooter>
-            <Button as={Link} href="/tools" className="bg-orange text-white font-semibold">
-              <FaTools /> Discover Now
+          <CardBody className="text-center text- mt-4">
+            <p className="text-lg leading-relaxed">
+              We leverage cutting-edge AI and extensive automotive databases to bring you the most relevant car recommendations. Our real-time market analysis tools examine trends, vehicle specifications, and your budget to offer personalized suggestions.
+            </p>
+            <p className="mt-4 text-gray-400">
+              With our technology, your car-buying experience becomes smarter, faster, and tailored precisely to your needs. Explore the power of data-driven insights and find your ideal vehicle today.
+            </p>
+          </CardBody>
+          <CardFooter className="flex justify-center mt-6">
+            <Button as={Link} href="/tools" className="bg-orange hover:bg-orange text-white font-semibold px-4 py-2 flex items-center space-x-2">
+              <FaTools className="text-xl" />
+              <span>Discover Now</span>
             </Button>
           </CardFooter>
         </Card>
+
       </div>
 
       <div className="bg-orange p-4 flex flex-col items-center">
-        <h2 className="bg-carbon text-xl text-white font-bold rounded-md inline-block px-4 py-1">
+        <h2 className="bg-carbon shadow-lg text-xl text-white font-bold rounded-md inline-block px-4 py-1">
           Our Tools
         </h2>
 
         <p className="text-center text-white max-w-md mt-2">
-          Explore our AI-driven tools designed to give you the best car-buying experience. From analyzing market trends to personalized recommendations, we’ve got you covered.
+          Discover our suite of AI-driven tools designed to enhance your car-buying journey. Whether you’re looking for in-depth market analysis or personalized recommendations, we have innovative solutions to meet your needs.
         </p>
 
         <div className="gap-4 flex flex-col md:flex-row space-between pt-4">
-          <Card>
-            <CardHeader>
+          <Card className="bg-carbon text-white rounded-lg shadow-lg">
+            <CardHeader className="flex items-center space-x-2 border-b">
+              <IoBarChart className="text-orange-500 text-xl" />
               <h3 className="text-lg font-bold">
                 Market Analyzer
               </h3>
             </CardHeader>
             <CardBody>
-              <p className="text-gray-700">
-                Get detailed insights into the car market, including price trends, vehicle demand, and more to help make informed decisions.
+              <p className="text-gray-300">
+                Dive into comprehensive market insights with our Market Analyzer. Track price fluctuations, assess vehicle demand, and view historical data to make smarter buying decisions. Our tool provides up-to-date statistics and actionable trends.
               </p>
             </CardBody>
             <CardFooter>
-              <Button as={Link} href="/tools/vin-analyzer" className="m-auto bg-carbon text-white">
-                <VscGraphLine /> Try Now
+              <Button as={Link} href="/tools/vin-analyzer" className="m-auto bg-white text-carbon hover:bg-gray-600">
+                <VscGraphLine className="inline-block mr-2" /> Try Now
               </Button>
             </CardFooter>
           </Card>
 
-          <Card>
-            <CardHeader>
+          <Card className="bg-carbon text-white rounded-lg shadow-lg">
+            <CardHeader className="flex items-center space-x-2 border-b">
+              <BsRobot className="text-orange-500 text-xl" />
               <h3 className="text-lg font-bold">
                 CarBot
               </h3>
             </CardHeader>
             <CardBody>
-              <p className="text-gray-700">
-                Chat with our AI-powered CarBot to find the perfect car based on your preferences and budget in real-time.
+              <p className="text-gray-300">
+                Engage with CarBot, our AI-powered virtual assistant. CarBot helps you explore vehicle options based on your preferences and budget. Enjoy real-time conversations and receive tailored suggestions for the best car deals.
               </p>
             </CardBody>
             <CardFooter>
-              <Button as={Link} href="/tools/carbot" className="m-auto bg-carbon text-white">
-                <BsRobot /> Chat Now
+              <Button as={Link} href="/tools/carbot" className="m-auto bg-white text-carbon">
+                <BsRobot className="inline-block mr-2" /> Chat Now
               </Button>
             </CardFooter>
           </Card>
 
-          <Card>
-            <CardHeader>
+          <Card className="bg-carbon text-white rounded-lg shadow-lg">
+            <CardHeader className="flex items-center space-x-2 border-b">
+              <GrServices className="text-orange-500 text-xl" />
               <h3 className="text-lg font-bold">
                 Custom Solutions
               </h3>
             </CardHeader>
             <CardBody>
-              <p className="text-gray-700">
-                Need something more specific? We provide custom solutions tailored to your automotive needs. Contact us today!
+              <p className="text-gray-300">
+                Looking for something more tailored? Our Custom Solutions offer personalized services to meet your unique automotive needs. From specialized reports to bespoke analysis, contact us to discuss how we can assist you.
               </p>
             </CardBody>
             <CardFooter>
-              <Button as={Link} href="/contact" className="m-auto bg-carbon text-white">
-                <GrContact /> Contact Now
+              <Button as={Link} href="/contact" className="m-auto bg-white text-carbon">
+                <GrContact className="inline-block mr-2" /> Contact Now
               </Button>
             </CardFooter>
           </Card>
         </div>
       </div>
-
     </div>
   );
 }
